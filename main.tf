053d488e6ff807355c4051cc7792cca41b1893f2
Hugo Ernandes desk <hugo.ernandes@hotmail.com>
2021-10-11 17:01:39 -0300

    aprendendo data

--- main.tf
+++ main.tf
@@ -2,11 +2,20 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "web" {
-  ami           = "ami-09e67e426f25ce0d7" #ami ubunto 20
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.ubuntu.id # "ami-09e67e426f25ce0d7" #ami ubunto 20
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["vpc-0d48f0e5799777077"] #vpc Hugo
 
   tags = {
     Name = "MinhaPrimeiraMaquinaHugo"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-*"]
   }
 }
