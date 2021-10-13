4cd12b8829970c0524efc797efb77336fd5b0c6b
Hugo Ernandes <hugo.ernandes@hotmail.com>
2021-10-13 18:22:02 +0000

    alterando o aquivo de output

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "ssh_conect" {
   value = [
     for key, item in aws_instance.web :
-      "ec2 ${key+1} - ${item.private_ip} - ssh -i ~/.ssh/id_rsa ubuntu@${item.public_ip}"
+      "ec2 ${key+1} - ${item.private_ip} - ssh -i ~/.ssh/id_rsa ubuntu@${item.private_ip}"
   ]
 }
