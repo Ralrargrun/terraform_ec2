a5374a866b429edc7b86f3e7222289aced98e6e0
Hugo Ernandes desk <hugo.ernandes@hotmail.com>
2021-10-13 17:41:38 -0300

    adicionando document db

--- sg.tf
+++ sg.tf
@@ -35,28 +35,3 @@ resource "aws_security_group" "permitir_ssh" {
     Name = "permitir_ssh"
   }
 }
-
-resource "aws_security_group" "acesso_mysql" {
-  name        = "acesso_mysql"
-  description = "Habilita porta padrao mysql"
-  vpc_id      = "vpc-0d48f0e5799777077"
-
-  ingress = [
-    {
-      description      = "Porta padrao mysql"
-      from_port        = 3306
-      to_port          = 3306
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = null,
-      security_groups  = null,
-      self             = null
-    }
-  ]
-
-  tags = {
-    Name = "acesso_mysql"
-  }
-}
-
