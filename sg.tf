ec5ea0cec46e97a18c23b2b19dbf54e0eab3ce31
Hugo Ernandes desk <hugo.ernandes@hotmail.com>
2021-10-13 16:33:07 -0300

    criando rds

--- sg.tf
+++ sg.tf
@@ -35,3 +35,27 @@ resource "aws_security_group" "permitir_ssh" {
     Name = "permitir_ssh"
   }
 }
+
+resource "aws_security_group" "acesso_mysql" {
+  name        = "acesso_mysql"
+  description = "Habilita porta padrao mysql"
+
+  ingress = [
+    {
+      description      = "Porta padrao mysql"
+      from_port        = 3306
+      to_port          = 3306
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids = null,
+      security_groups: nulla,
+      self: null
+    }
+  ]
+
+  tags = {
+    Name = "acesso_mysql"
+  }
+}
+
